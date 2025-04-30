f9684606f8743304a40b2ca5fd24aa2b57543489
RAE-47 <robin.ertas@elev.ga.lbs.se>
2025-04-30 13:18:30 +0200

    Update Movement.cs

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,8 +8,18 @@ public class Movement : MonoBehaviour
     public Speeds CurrentSpeed;
     //                       0      1      2       3      4
     float[] SpeedValues = { 8.6f, 10.4f, 12.96f, 15.6f, 19.27f };
+
+    public Transform GroundCheckTransform;
+    public float GrounCheckRadius;
+    public LayerMask GroundMask;
+
     void Update()
     {
         transform.position += Vector3.right * SpeedValues[(int)CurrentSpeed] * Time.deltaTime;
+        if(Input.GetMouseButton(0) )
+    }
+    bool OnGround() 
+    {
+    return Physics2D.OverlapCircle(GroundCheckTransform.position, GrounCheckRadius, GroundMask);
     }
 }
